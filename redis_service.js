3aab6397cd790cf5ea5a400459ae85c49d1f048a
killerdejavu <mananshah9328@gmail.com>
2017-12-02 22:37:37 +0530

    putting some logs for gran test

--- redis_service.js
+++ redis_service.js
@@ -16,12 +16,12 @@ module.exports = {
     getCurrentSong: function (callback) {
         this.getCurrentSongDetails(function (current_song_id, starting_timestamp, ending_timestamp) {
             var current_time = moment();
-            var should_current_song_not_change = current_song_id && current_time.isBetween(moment(starting_timestamp), moment(ending_timestamp), 'second');
             console.log('current time', current_time);
-            console.log('starting timestamp', starting_timestamp);
-            console.log('ending timestamp', ending_timestamp);
+            console.log('starting timestamp', moment(starting_timestamp));
+            console.log('ending timestamp', moment(ending_timestamp));
             console.log('should change song ? - ', !should_current_song_not_change);
             console.log('------------');
+            var should_current_song_not_change = current_song_id && current_time.isBetween(moment(starting_timestamp), moment(ending_timestamp), 'second');
             if (!should_current_song_not_change) {
                 this.popFirstSongFromPlaylist(function (err, song_id) {
                     this.setCurrentSongDetails(song_id);
