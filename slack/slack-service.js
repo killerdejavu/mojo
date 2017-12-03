d237d19f5e16a4cfa6b5efe17f77f215ea8a6364
Roopendra <roopendratalekar@gmail.com>
2017-12-03 20:16:16 +0530

    error handling in youtube-service works, adding proper message to slack message

--- slack/slack-service.js
+++ slack/slack-service.js
@@ -33,20 +33,15 @@ function handleIncomingSlackData(slack_data) {
         const parsed_youtube_links = parseYoutubeLinksFromText(slack_data.text);
         if (parsed_youtube_links.length > 0) {
             parsed_youtube_links.forEach(function (youtube_link) {
-                try {
-                    youtubeService.fetchSongAndAddToStore(youtube_link).then((songData) => {
-                        return playlistService.addSong(songData.songId).then(() => {
-                            sendDataToSlackChannel(`The song ${songData.meta.title} has been added to the playlist.. `);
-                            debug('playing song from slack %O', songData)
-                        });
-                    }).catch((err) => {
-                        sendDataToSlackChannel(`We could not add the song: ${songData.meta.title}.. please try again`);
-                        debug('an error occured: %s', err);
+                youtubeService.fetchSongAndAddToStore(youtube_link).then((songData) => {
+                    return playlistService.addSong(songData.songId).then(() => {
+                        sendDataToSlackChannel(`The song ${songData.meta.title} has been added to the playlist.. `);
+                        debug('playing song from slack %O', songData)
                     });
-                } catch (err) {
-                    sendDataToSlackChannel(`We could not add the song: ${songData.meta.title}.. please try again`);
+                }).catch((err) => {
+                    sendDataToSlackChannel(`We could not add the song: ${youtube_link}.. please try again`);
                     debug('an error occured: %s', err);
-                }
+                });
             });
         }
     }
