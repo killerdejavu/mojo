8fdf96dde5383d99a137c281ac530528f39a9170
killerdejavu <mananshah9328@gmail.com>
2017-12-03 17:44:20 +0530

    playing song from slack message

--- slack/slack-service.js
+++ slack/slack-service.js
@@ -1,4 +1,5 @@
 var youtubeService = require('../youtube/youtube-service');
+var playlistService = require('../playlist/playlist-service');
 
 function isValidSlackRequest(token) {
     return token === "1avbAdJLErZeZ7VoclM0um2b";
@@ -8,6 +9,7 @@ function parseYoutubeLinksFromText(str) {
     const regex = /(?:https?:\/\/)(?:www\.)?(?:youtube|youtu)\.(?:be|com)\/[^\s]+/g;
     let m;
 
+    let youtube_links = [];
     while ((m = regex.exec(str)) !== null) {
         // This is necessary to avoid infinite loops with zero-width matches
         if (m.index === regex.lastIndex) {
@@ -16,17 +18,26 @@ function parseYoutubeLinksFromText(str) {
 
         // The result can be accessed through the `m`-variable.
         m.forEach((match, groupIndex) => {
-            console.log(`Found match, group ${groupIndex}: ${match}`);
+            youtube_links.push(match);
         });
     }
+
+    return youtube_links
 }
 
 function handleIncomingSlackData(slack_data) {
     if (isValidSlackRequest(slack_data.token)) {
-        parseYoutubeLinksFromText(slack_data.text);
-        return 'Success'
+        const parsed_youtube_links = parseYoutubeLinksFromText(slack_data.text);
+        if (parsed_youtube_links.length > 0) {
+            parsed_youtube_links.forEach(function (youtube_link) {
+                youtubeService.fetchSongAndAddToStore(youtube_link).then((songData) => {
+                    return playlistService.addSong(songData.songId).then(() => {
+                        console.log('playing song from slack', songData)
+                    });
+                });
+            });
+        }
     }
-    return 'Failure';
 }
 
 
