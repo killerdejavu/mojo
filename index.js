0d453c1d886fb861c872ffd0efa77a902cf0fe8e
killerdejavu <mananshah9328@gmail.com>
2017-12-03 14:16:30 +0530

    tring out the body parser

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
 const radioService = require('./radio/radio-service');
 const playlistService = require('./playlist/playlist-service');
 const youtubeService = require('./youtube/youtube-service');
@@ -10,7 +12,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function (request, response) {
     response.render('index');
