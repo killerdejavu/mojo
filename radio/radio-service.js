99749ff6c4bee4bd354991bf5d84cbee304c0848
Manan Shah <manan.shah@zomato.com>
2019-01-16 14:08:35 +0530

    url fixes

--- radio/radio-service.js
+++ radio/radio-service.js
@@ -36,7 +36,7 @@ function setCurrentSongDetails(songId) {
                 JSON.stringify(songData),
                 (err, response) => {
                     if (err) return reject(err);
-                    slackService.sendMessage(`:sound: Now playing - *${songData.meta.title}*. Listen to it on <https://tiny.cc/rbox-radio|mojo-radio> :notes: :notes:`, null, config.SLACK_CHANNEL_NAME);
+                    slackService.sendMessage(`:sound: Now playing - *${songData.meta.title}*. Listen to it on <https://mojoradio.herokuapp.com/|mojo-radio> :notes: :notes:`, null, config.SLACK_CHANNEL_NAME);
                     resolve(songData);
                 });
         });
